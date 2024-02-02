694ca566931a0fd23f95f952ca7e0b25b12853d2
Matthew Rawlings <mattcanemail@gmail.com>
2024-02-01 22:51:21 -0800

    feat(cli): bad cli init func

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	"github.com/awlsring/texit/internal/app/ui/adapters/primary/cli"
 	"github.com/awlsring/texit/internal/app/ui/adapters/primary/cli/handler"
 	api_gateway "github.com/awlsring/texit/internal/app/ui/adapters/secondary/gateway/api"
-	"github.com/awlsring/texit/internal/app/ui/config"
 	"github.com/awlsring/texit/internal/app/ui/core/service/api"
 	"github.com/awlsring/texit/pkg/gen/texit"
 )
@@ -33,8 +34,19 @@ func initClient(address string, key string) texit.Invoker {
 	return c
 }
 
+func isInitCommand(args []string) bool {
+	return len(args) > 1 && args[1] == "init"
+}
+
 func main() {
-	cfg, err := config.LoadFromFile("cli-config.yaml")
+	if isInitCommand(os.Args) {
+		cli.InitDefaultConfig()
+		fmt.Println("Texit default config has been initialized. Please edit the file at ~/.texit/config.yaml to set your server address and api key.")
+		return
+	}
+
+	cli.MakeTexitDir()
+	cfg, err := cli.LoadConfig()
 	panicOnErr(err)
 	client := initClient(cfg.Api.Address, cfg.Api.ApiKey)
 
---
+++ internal/app/ui/adapters/primary/cli/init.go
@@ -0,0 +1,60 @@
+package cli
+
+import (
+	"os"
+
+	"github.com/awlsring/texit/internal/app/ui/config"
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	configOverrideFlag = "TEXIT_CONFIG"
+	defaultConfigPath  = ".texit/config.yaml"
+)
+
+func texitDir() string {
+	return os.ExpandEnv("$HOME/" + ".texit")
+}
+
+func MakeTexitDir() {
+	if _, err := os.Stat(texitDir()); os.IsNotExist(err) {
+		os.MkdirAll(texitDir(), 0755)
+	}
+}
+
+func ensurePathExists(path string) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		os.MkdirAll(path, 0755)
+	}
+}
+
+func configPath() string {
+	return os.ExpandEnv("$HOME/" + defaultConfigPath)
+}
+
+func InitDefaultConfig() {
+	cfg := config.Config{
+		Api: config.ApiConfig{
+			Address: "http://myserver:7032",
+			ApiKey:  "changeme",
+		},
+	}
+	path := configPath()
+	y, _ := yaml.Marshal(cfg)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		os.WriteFile(path, y, 0644)
+	}
+}
+
+func LoadConfig() (*config.Config, error) {
+	configPath := configPath()
+	if override := os.Getenv(configOverrideFlag); override != "" {
+		configPath = override
+	}
+
+	cfg, err := config.LoadFromFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
