5d17488170fd0f310c7857b682250e58b52b4f50
Matthew Rawlings <mattcanemail@gmail.com>
2024-02-07 19:53:34 -0800

    feat(cli): ec2 provider init

--- internal/app/ui/adapters/primary/cli/handler/provider_init.go
+++ internal/app/ui/adapters/primary/cli/handler/provider_init.go
@@ -17,9 +17,61 @@ func (h *Handler) ProviderInit(c *cli.Context) error {
 	switch t {
 	case provider.TypeAwsEcs:
 		return h.initAwsEcsProvider(c)
+	case provider.TypeAwsEc2:
+		return h.initAwsEc2Provider(c)
 	default:
 		return fmt.Errorf("unknown provider type: %s", t.String())
 	}
+}
+
+func (h *Handler) initAwsEc2Provider(ctx *cli.Context) error {
+	fmt.Println("To use an AWS EC2 provider, you must have an AWS account and have the AWS CLI installed and configured.")
+	fmt.Println("For more on this information, see: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-quickstart.html")
+	fmt.Println("")
+	fmt.Println("Once you have the AWS CLI installed and configured, you can run the following commands to create the required resources:")
+	fmt.Println("")
+	fmt.Println("First, create an IAM user that the provider will use")
+	fmt.Println("aws iam create-user --user-name texit-ec2-provider")
+	fmt.Println("")
+	fmt.Println("Then create a JSON file with a policy document with the following command.")
+	fmt.Println("")
+	fmt.Println(`echo '{
+	"Version": "2012-10-17",
+	"Statement": [
+		{
+			"Effect": "Allow",
+			"Action": [
+                "ec2:DescribeImages",
+                "ec2:RunInstances",
+                "ec2:StartInstances",
+                "ec2:StopInstances",
+                "ec2:TerminateInstances",
+                "ec2:DescribeInstances"
+			],
+			"Resource": "*"
+		}
+	]
+}' > texit-ec2-provider-policy.json`)
+	fmt.Println("")
+	fmt.Println("Next, create the IAM policy from the JSON file. Note the ARN of the policy as it will be used next.")
+	fmt.Println("aws iam create-policy --policy-name texit-ec2-provider-policy --policy-document file://texit-ec2-provider-policy.json")
+	fmt.Println("")
+	fmt.Println("Attach the policy to the user")
+	fmt.Println("aws iam attach-user-policy --user-name texit-ec2-provider --policy-arn <policy-arn>")
+	fmt.Println("")
+	fmt.Println("Create an access key for the user")
+	fmt.Println("aws iam create-access-key --user-name texit-ec2-provider")
+	fmt.Println("")
+	fmt.Println("Finally, configure the provider with the access key and secret key in the texit configuration file")
+	fmt.Print(`
+providers:
+  ...
+  - type: aws-ec2
+    accessKey: <THE ACCESS KEY FOR THE USER>
+    secretKey: <THE SECRET KEY FOR THE USER>
+    name: <SOME UNIQUE NAME TO CALL THIS PROVIDER>
+`)
+	return nil
 }
 
 func (h *Handler) initAwsEcsProvider(ctx *cli.Context) error {
