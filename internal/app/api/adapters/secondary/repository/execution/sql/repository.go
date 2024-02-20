638d50e08df2675904701603cd7004db9cc11956
Matthew Rawlings <contact@matthewrawlings.com>
2024-02-19 21:52:42 -0800

    feat(api): dump old execution records

--- internal/app/api/adapters/secondary/repository/execution/sql/repository.go
+++ internal/app/api/adapters/secondary/repository/execution/sql/repository.go
@@ -2,9 +2,11 @@ package sql_execution_repository
 
 import (
 	"context"
+	"time"
 
 	"github.com/awlsring/texit/internal/app/api/ports/repository"
 	"github.com/awlsring/texit/internal/pkg/interfaces"
+	"github.com/rs/zerolog/log"
 	_ "modernc.org/sqlite"
 )
 
@@ -21,6 +23,21 @@ func New(db interfaces.SqlDatabase) repository.Execution {
 func (r *SqlExecutionRepository) Init(ctx context.Context) error {
 	err := r.initTables(ctx)
 	if err != nil {
+		return err
+	}
+	err = r.dumpOldExecutions(ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *SqlExecutionRepository) dumpOldExecutions(ctx context.Context) error {
+	daysAgo5 := time.Now().AddDate(0, 0, -5)
+	query := "DELETE FROM executions WHERE finished_at < $1"
+	_, err := r.db.ExecContext(ctx, query, daysAgo5)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to delete node from sqlite database")
 		return err
 	}
 	return nil
